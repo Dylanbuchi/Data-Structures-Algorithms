4c799bf0108f84cbbd5ad01f90f898e0938d5a05
Dylan <devybuchi@gmail.com>
2020-03-16 08:35:21 -0300

    first commit

---
+++ app/App.java
@@ -0,0 +1,11 @@
+/**
+ * App
+ */
+package app;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/data_structure/Array.java
@@ -0,0 +1,11 @@
+package app.data_structure;
+
+/**
+ * Array
+ */
+public class Array {
+
+    public Array(int size) {
+
+    }
+}
