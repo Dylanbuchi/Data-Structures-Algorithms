3d99214c9817bbc9d86c95b47cca52514afce379
Dylan <devybuchi@gmail.com>
2020-03-16 09:50:56 -0300

    added Arrays

--- app/App.java
+++ app/App.java
@@ -3,9 +3,20 @@
  */
 package app;
 
+import app.data_structure.Array;
+
 public class App {
 
     public static void main(String[] args) {
-ss
+        Array a = new Array(2);
+        a.insert(10);
+        a.insert(20);
+        a.insert(30);
+
+        a.removeAt(1);
+
+        a.print();
+
+        System.out.println("index of number 10 : " + a.indexOf(10));
     }
 }
--- app/data_structure/Array.java
+++ app/data_structure/Array.java
@@ -1,12 +1,60 @@
 package app.data_structure;
 
+import java.util.Arrays;
+
 /**
  * Array
  */
 public class Array {
-    private int a;
+
+    private int[] array;
+    private int count;
 
     public Array(int size) {
 
+        this.array = new int[size];
+        this.count = 0;
+    }
+
+    public void removeAt(int index) {
+
+        if (index >= count || index < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = index; i < count; i++) {
+            array[i] = array[i + 1];
+        }
+        count--;
+    }
+
+    public void insert(int num) {
+        if (count == array.length) {
+            int[] biggerArray = new int[count * 2];
+
+            for (int i = 0; i < array.length; i++) {
+                biggerArray[i] = array[i];
+
+            }
+            array = biggerArray;
+        }
+        array[count++] = num;
+    }
+
+    public int indexOf(int num) {
+        for (int i = 0; i < count; i++) {
+            if (array[i] == num) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(array[i]);
+
+        }
     }
 }
