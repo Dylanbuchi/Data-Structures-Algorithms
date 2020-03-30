28990960603a270a786cf183a9a9b300f00ffa3f
Dylan <devybuchi@gmail.com>
2020-03-30 09:06:33 -0300

    added HashTablle

--- app/App.java
+++ app/App.java
@@ -3,19 +3,14 @@
  */
 package app;
 
-import app.data_structure.PriorityQueue;
+import app.data_structure.HashTable;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        PriorityQueue pq = new PriorityQueue();
-        pq.insert(1);
-        pq.insert(2);
-        pq.insert(3);
-        pq.insert(4);
-        System.out.println(pq);
-        pq.remove();
-        System.out.println(pq);
+        HashTable table = new HashTable();
+        table.put(6, "sd");
+
     }
 
 }
--- app/data_structure/LinkedList.java
+++ app/data_structure/LinkedList.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
  * LinkedList
  */
 public class LinkedList {
+
     private Node first;
     private Node last;
     private int size;
