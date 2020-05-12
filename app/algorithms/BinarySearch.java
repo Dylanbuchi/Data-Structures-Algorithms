a62c39fa5c9675f7c1b7b1b5d3e64378939ef278
Dylan <devybuchi@gmail.com>
2020-05-12 11:17:19 -0300

    added binary search

---
+++ app/algorithms/BinarySearch.java
@@ -0,0 +1,47 @@
+package app.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * BinarySearch
+ */
+public class BinarySearch {
+
+    public static int binarySearch(int x, int[] array) {
+        int front, mid, back, midValue;
+
+        front = 0;
+        back = array.length - 1;
+
+        while (front <= back) {
+            mid = (front + back) / 2;
+            midValue = array[mid];
+
+            if (midValue == x) {
+                return mid;
+
+            } else if (midValue > x)
+                back--;
+            else {
+                front++;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = { 34, 56, 7, 83, 0, -56, 56, 789, 8 };
+        Arrays.sort(array);
+
+        int x = -56;
+        int index = (binarySearch(x, array));
+
+        String message = index != -1 ? "the index for value " + x + " is " + index
+                : "the value does not exist in this array";
+
+        System.out.println(message);
+    }
+}
--- app/algorithms/Linear.java
+++ app/algorithms/Linear.java
@@ -2,20 +2,7 @@ package app.algorithms;
 
 public class Linear {
 
-    public static void main(String[] args) {
-
-        int[] array = { 1, 2, 3, 4, 5 };
-        int x = 90;
-        int index = findIndexOfValue(x, array);
-
-        String message = index != -1 ? "the index for value " + x + " is " + index
-                : "the value does not exist in this array";
-
-        System.out.println(message);
-
-    }
-
-    public static int findIndexOfValue(int x, int[] array) {
+    public static int linearSearch(int x, int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == x) {
 
@@ -24,6 +11,19 @@ public class Linear {
 
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = { 13, 24, 356, 47, 58 };
+        int x = 47;
+        int index = linearSearch(x, array);
+
+        String message = index != -1 ? "the index for value " + x + " is " + index
+                : "the value does not exist in this array";
+
+        System.out.println(message);
+
     }
 
 }
